198769b750581062208593a83c07719da29809b8
Doc1998 <docmp98@gmail.com>
2026-07-20 15:48:07 +0100

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "Tipperary Groundworks",
+    "email": "tipperarygroundworks@gmail.com",
+    "phoneForTel": "+353873566934",
+    "phoneFormatted": "+353873566934",
+    "address": {
+        "lineOne": "Tipperary",
+        "lineTwo": "",
+        "city": "Tipperary",
+        "state": "Tipperary",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/profile.php?id=61587139168472",
+        "instagram": "instagram.com"
+    },
+    "domain": "tgw.ie"
+};
